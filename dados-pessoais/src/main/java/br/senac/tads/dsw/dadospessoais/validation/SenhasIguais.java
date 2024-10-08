4f9f44b5587ab5e3d061a8b79c540f975a024552
ftsuda-senac <ftsuda.senac@gmail.com>
2024-10-08 20:11:53 -0300

    Tela index

--- dados-pessoais/src/main/java/br/senac/tads/dsw/dadospessoais/validation/SenhasIguais.java
+++ dados-pessoais/src/main/java/br/senac/tads/dsw/dadospessoais/validation/SenhasIguais.java
@@ -9,9 +9,9 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Documented
-@Constraint(validatedBy = SenhasIguaisValidator.class)
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = SenhasIguaisValidator.class)
 public @interface SenhasIguais {
 
     String message() default "A senha e a repetição devem ser iguais";
